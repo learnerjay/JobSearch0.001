a19f656703bc71ad6267f513bac11d4df273a7c5
root <root@ubuntu-24.localhost>
2025-09-08 05:19:21 +0000

    Logs added by LogWrite

--- src/main/java/com/jobportal/dao/SearchDAO.java
+++ src/main/java/com/jobportal/dao/SearchDAO.java
@@ -15,16 +15,24 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class SearchDAO {
+logger.error("ENTERING: searchByTerm() method with database operations");
+logger.error("ENTERING: searchByTerm() method, parameters: no parameters");
+logger.debug("ENTERING: searchByTerm() method");
+logger.error("ENTERING: searchByTerm() method with database operations");
 	
+logger.error("ENTERING: searchByTerm() method, parameters: className={}, term={}, p={}", className, term, p);
 	@Autowired
+logger.debug("ENTERING: searchByTerm() method");
 	MongoTemplate template;
 
 	public <T> List<T> searchByTerm(Class className, String term, Pageable p)
+logger.error("Database operation: FIND - find");
 	{
 		TextCriteria criteria = TextCriteria.forDefaultLanguage()
 				.matchingAny(term.split("\\s+"));  // handles multiple words
 
 		Query query = TextQuery.queryText(criteria)
+logger.warn("EXITING: searchByTerm() method - Successfully completed");
 				.with(Sort.by(Sort.Direction.DESC, "updatedOn"))
 				.with(p);
 
